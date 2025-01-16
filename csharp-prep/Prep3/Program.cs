745316fd01a013427c9b4c7d31071a3a8b5648a9
EMC2001 <emilianocastellanos0@gmail.com>
2025-01-15 19:03:38 -0700

    Guess number prep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        int number = int.Parse(Console.ReadLine());
+
+        int guess = 0;
+    
+        while (guess != number)
+        {
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if(number > guess)
+            Console.WriteLine("Higher");
+
+            else if(number < guess)
+            Console.WriteLine("Lower");
+
+            else
+            Console.WriteLine("You guessed it!");
+
+        } 
     }
 }
